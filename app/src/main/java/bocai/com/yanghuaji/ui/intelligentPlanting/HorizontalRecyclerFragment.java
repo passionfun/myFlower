65990a48795dcadbc591423678802eb038ab4f52
danyuanfei221 <178731754@qq.com>
2017-12-04 09:35:28 +0800

    配置长牙sdk，修改一些bug

--- app/src/main/java/bocai/com/yanghuaji/ui/intelligentPlanting/HorizontalRecyclerFragment.java
+++ app/src/main/java/bocai/com/yanghuaji/ui/intelligentPlanting/HorizontalRecyclerFragment.java
@@ -1,5 +1,4 @@
 package bocai.com.yanghuaji.ui.intelligentPlanting;
-
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
@@ -7,9 +6,10 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
+import com.google.gson.Gson;
 import java.util.List;
-
+import java.util.Timer;
+import java.util.TimerTask;
 import bocai.com.yanghuaji.R;
 import bocai.com.yanghuaji.base.Application;
 import bocai.com.yanghuaji.base.GlideApp;
@@ -17,6 +17,7 @@ import bocai.com.yanghuaji.base.RecyclerAdapter;
 import bocai.com.yanghuaji.base.common.Common;
 import bocai.com.yanghuaji.base.presenter.PrensterFragment;
 import bocai.com.yanghuaji.model.EquipmentRspModel;
+import bocai.com.yanghuaji.model.PlantStatusModel;
 import bocai.com.yanghuaji.presenter.intelligentPlanting.IntelligentPlantContract;
 import bocai.com.yanghuaji.presenter.intelligentPlanting.IntelligentPlantPresenter;
 import bocai.com.yanghuaji.ui.intelligentPlanting.recyclerHelper.GalleryLayoutManager;
@@ -24,31 +25,29 @@ import bocai.com.yanghuaji.ui.intelligentPlanting.recyclerHelper.ScaleTransforme
 import bocai.com.yanghuaji.util.persistence.Account;
 import butterknife.BindView;
 import butterknife.OnClick;
-
+import xpod.longtooth.LongTooth;
+import xpod.longtooth.LongToothAttachment;
+import xpod.longtooth.LongToothServiceResponseHandler;
+import xpod.longtooth.LongToothTunnel;
 /**
  * 作者 yuanfei on 2017/11/15.
  * 邮箱 yuanfei221@126.com
  */
-
 public class HorizontalRecyclerFragment extends PrensterFragment<IntelligentPlantContract.Presenter>
-        implements IntelligentPlantContract.View {
+        implements IntelligentPlantContract.View  {
     @BindView(R.id.recycler)
     RecyclerView mRecyclerView;
-
     @BindView(R.id.tv_current)
     TextView mCurrentNum;
-
     @BindView(R.id.tv_total)
     TextView mTotalNum;
-
     private int page = 1;
+    public static final String TAG = HorizontalRecyclerFragment.class.getName();
     private RecyclerAdapter<EquipmentRspModel.ListBean> mAdapter;
-
     @Override
     protected int getContentLayoutId() {
         return R.layout.fragment_horizontal_recycler;
     }
-
     @Override
     protected void initWidget(View root) {
         super.initWidget(root);
@@ -59,10 +58,11 @@ public class HorizontalRecyclerFragment extends PrensterFragment<IntelligentPlan
         mAdapter.setListener(new RecyclerAdapter.AdapterListenerImpl<EquipmentRspModel.ListBean>() {
             @Override
             public void onItemClick(RecyclerAdapter.ViewHolder holder, EquipmentRspModel.ListBean plantModel) {
-                Log.d("test", Common.Constance.H5_BASE + "product.html?id=" + plantModel.getId());
-                PlantingDateAct.show(getContext(), Common.Constance.H5_BASE + "product.html?id=" + plantModel.getId());
+                Log.d("test", Common.Constance.H5_BASE + "product.html?id="+ plantModel.getId());
+                PlantingDateAct.show(getContext(), Common.Constance.H5_BASE + "product.html?id="+ plantModel.getId());
             }
         });
+
         layoutManager.setOnItemSelectedListener(new GalleryLayoutManager.OnItemSelectedListener() {
             @Override
             public void onItemSelected(RecyclerView recyclerView, View item, int position) {
@@ -70,15 +70,14 @@ public class HorizontalRecyclerFragment extends PrensterFragment<IntelligentPlan
             }
         });
         mCurrentNum.setText("1");
-    }
 
+    }
     @Override
     protected void initData() {
         super.initData();
         page = 1;
         mPresenter.getAllEquipments(Account.getToken(), "10", page + "");
     }
-
     @Override
     public void getAllEquipmentsSuccess(List<EquipmentRspModel.ListBean> listBeans) {
         if (page == 1) {
@@ -91,58 +90,41 @@ public class HorizontalRecyclerFragment extends PrensterFragment<IntelligentPlan
         }
         mTotalNum.setText(String.valueOf(listBeans.size()));
     }
-
     @Override
     protected IntelligentPlantContract.Presenter initPresenter() {
         return new IntelligentPlantPresenter(this);
     }
-
-
     class Adapter extends RecyclerAdapter<EquipmentRspModel.ListBean> {
-
         @Override
         protected int getItemViewType(int position, EquipmentRspModel.ListBean plantModel) {
             return R.layout.item_main_horizontal;
         }
-
         @Override
         protected ViewHolder<EquipmentRspModel.ListBean> onCreateViewHolder(View root, int viewType) {
-
             return new MyViewHolder(root);
         }
-
-
         class MyViewHolder extends RecyclerAdapter.ViewHolder<EquipmentRspModel.ListBean> {
             @BindView(R.id.frame_setting)
             FrameLayout mSettingView;
-
             @BindView(R.id.img_setting)
             ImageView mSetting;
-
             @BindView(R.id.tv_equipment_name)
             TextView mEquipmentName;
-
             @BindView(R.id.tv_plant_name)
             TextView mPlantName;
-
             @BindView(R.id.tv_group_name)
             TextView mGroupName;
-
             @BindView(R.id.tv_plant_time)
             TextView mTime;
-
             @BindView(R.id.image)
             ImageView mImage;
-
             private LinearLayout lldata;
-            private EquipmentRspModel.ListBean mModel;
+            private EquipmentRspModel.ListBean  mModel;
             private boolean isShowSetting = false;
-
             public MyViewHolder(View itemView) {
                 super(itemView);
                 lldata = itemView.findViewById(R.id.ll_data);
             }
-
             @Override
             protected void onBind(final EquipmentRspModel.ListBean plantModel) {
                 mModel = plantModel;
@@ -150,42 +132,55 @@ public class HorizontalRecyclerFragment extends PrensterFragment<IntelligentPlan
                 mEquipmentName.setText(plantModel.getEquipName());
                 mPlantName.setText(plantModel.getPlantName());
                 mGroupName.setText(plantModel.getGroupName());
-                mTime.setText(plantModel.getDays() + "");
+                mTime.setText(plantModel.getDays()+"");
                 GlideApp.with(getContext())
                         .load(plantModel.getPhoto())
                         .centerCrop()
                         .placeholder(R.mipmap.img_main_empty)
                         .into(mImage);
+                Timer timer = new Timer();
+                TimerTask task = new TimerTask() {
+                    @Override
+                    public void run() {
+                        Gson gson = new Gson();
+                        PlantStatusModel model = new PlantStatusModel("1","getStatus","1",plantModel.getPSIGN(),"1",plantModel.getPid());
+                        String request = gson.toJson(model);
+                        LongTooth.request(plantModel.getLTID(),"longtooth", LongToothTunnel.LT_ARGUMENTS,request.getBytes(),
+                                0,request.getBytes().length,null,new LongToothResponse());
+                    }
+                };
+                timer.schedule(task,5000,5000);
             }
-
             @OnClick(R.id.ll_data)
-            void onDataClick() {
-                Log.d("test", Common.Constance.H5_BASE + "product_data.html?id=" + mModel.getId());
-                PlantingDateAct.show(getContext(), Common.Constance.H5_BASE + "product_data.html?id=" + mModel.getId());
+            void onDataClick(){
+                Log.d("test",Common.Constance.H5_BASE + "product_data.html?id="+ mModel.getId());
+                PlantingDateAct.show(getContext(), Common.Constance.H5_BASE + "product_data.html?id="+ mModel.getId());
             }
-
             @OnClick(R.id.tv_setting_second)
-            void onSecondSettingClick() {
+            void onSecondSettingClick(){
                 String mEquipmentId = mModel.getId();
                 String mPlantId = mModel.getPid();
-                SecondSettingActivity.show(getContext(), mEquipmentId, mPlantId);
+                SecondSettingActivity.show(getContext(),mEquipmentId,mPlantId);
             }
-
-
             @OnClick(R.id.img_setting)
-            void onSettingClick() {
+            void onSettingClick(){
+                Log.d(TAG, "run: "+mModel.getPSIGN());
                 isShowSetting = !isShowSetting;
-                if (isShowSetting) {
+                if (isShowSetting){
                     mSettingView.setVisibility(View.VISIBLE);
                     mSetting.setImageResource(R.mipmap.img_close_main_horizontal);
-                } else {
+                }else {
                     mSettingView.setVisibility(View.GONE);
                     mSetting.setImageResource(R.mipmap.img_item_setting);
                 }
             }
         }
-
-
+        class LongToothResponse implements LongToothServiceResponseHandler {
+            @Override
+            public void handleServiceResponse(LongToothTunnel longToothTunnel, String s, String s1, int i, byte[] bytes, LongToothAttachment longToothAttachment) {
+                Log.d("shc", "horizoontalResponse: "+new String(bytes));
+                Application.showToast("horizontal" + new String(bytes));
+            }
+        }
     }
-
 }
