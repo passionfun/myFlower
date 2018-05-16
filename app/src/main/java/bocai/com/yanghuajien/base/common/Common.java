a4ae815e2a4b88f4a94c600b82cb7eb3edc81ea1
sunhengchao <1029651026@qq.com>
2018-05-16 16:40:33 +0800

    修改一些中文资源

--- app/src/main/java/bocai/com/yanghuajien/base/common/Common.java
+++ app/src/main/java/bocai/com/yanghuajien/base/common/Common.java
@@ -10,16 +10,10 @@ public class Common {
         // 手机号的正则,11位手机号
         String REGEX_MOBILE = "[1][3,4,5,7,8][0-9]{9}$";
 
-        // 测试服务器地址
-//        String API_URL = "http://121.41.128.239:8082/yhj/web/api/index.php/";
-        //正式服务器地址
-//        String API_URL = "http://47.98.46.78/web/api/index.php/";
         //英文版服务器地址
         String API_URL = "http://demo.188388.cn:8082/yhjen/web/api/index.php/";
 
-        //H5测试地址
-//        String H5_BASE = "http://121.41.128.239:8082/yhj/web/h5/yhj/";
-        //H5正式地址 http://47.98.46.78/web/h5/yhj/product.html?id=123
+        //H5英文版地址
         String H5_BASE = "http://47.98.46.78/web/h5/yhj/";
 
 
